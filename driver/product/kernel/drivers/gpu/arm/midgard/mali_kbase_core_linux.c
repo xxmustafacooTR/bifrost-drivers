7c253f569526ab29ed710fabdffcbf170553aa42
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2022-02-22 23:09:37 +0300

    Import Samsung Changes and Adaptive Power Policy

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -2067,6 +2067,9 @@ static long kbase_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				kctx);
 		break;
 	}
+	
+	if (!mali_exynos_ioctl(kctx, cmd, arg))
+		return 0;
 
 	dev_warn(kbdev->dev, "Unknown ioctl 0x%x nr:%d", cmd, _IOC_NR(cmd));
 
@@ -4288,6 +4291,8 @@ static int kbase_common_reg_map(struct kbase_device *kbdev)
 		err = -EINVAL;
 		goto out_ioremap;
 	}
+	
+	mali_exynos_coherency_reg_map();
 
 	return err;
 
@@ -4306,6 +4311,8 @@ static void kbase_common_reg_unmap(struct kbase_device * const kbdev)
 		kbdev->reg_start = 0;
 		kbdev->reg_size = 0;
 	}
+	
+	mali_exynos_coherency_reg_unmap();
 }
 #endif /* CONFIG_MALI_NO_MALI */
 
@@ -5364,6 +5371,8 @@ int kbase_sysfs_init(struct kbase_device *kbdev)
 		sysfs_remove_group(&kbdev->dev->kobj,
 			&kbase_attr_group);
 	}
+	
+	mali_exynos_sysfs_set_gpu_model_callback(&gpuinfo_show);
 
 	return err;
 }
@@ -5504,6 +5513,9 @@ static int kbase_device_resume(struct device *dev)
 	if (!kbdev)
 		return -ENODEV;
 
+	/* MALI_SEC_INTEGRATION */
+	mali_exynos_set_pm_state_resume_begin();
+
 	kbase_pm_resume(kbdev);
 
 #ifdef CONFIG_MALI_MIDGARD_DVFS
@@ -5515,6 +5527,10 @@ static int kbase_device_resume(struct device *dev)
 	if (kbdev->devfreq)
 		kbase_devfreq_enqueue_work(kbdev, DEVFREQ_WORK_RESUME);
 #endif
+
+	/* MALI_SEC_INTEGRATION */
+	mali_exynos_set_pm_state_resume_end();
+
 	return 0;
 }
 
@@ -5631,7 +5647,9 @@ static int kbase_device_runtime_idle(struct device *dev)
 	 * the autosuspend delay and so won't suspend the device immediately.
 	 */
 	pm_runtime_mark_last_busy(kbdev->dev);
-	return 0;
+	/* MALI_SEC_INTEGRATION */
+	/* Runtime IDLE must be return 1 for turn on next time by RuntimePM API!! */
+	return 1;
 }
 #endif /* KBASE_PM_RUNTIME */
 
@@ -5649,6 +5667,8 @@ static const struct dev_pm_ops kbase_pm_ops = {
 
 #if IS_ENABLED(CONFIG_OF)
 static const struct of_device_id kbase_dt_ids[] = {
+	/* MALI_SEC_INTEGRATION */
+	{ .compatible = "arm,mali", },
 	{ .compatible = "arm,malit6xx" },
 	{ .compatible = "arm,mali-midgard" },
 	{ .compatible = "arm,mali-bifrost" },
