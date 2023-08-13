e0dc9613f1ae3878051b07c75440fe76dfbade25
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2023-08-14 02:26:10 +0300

    Add Samsung Changes

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -35,7 +35,7 @@
 #include "backend/gpu/mali_kbase_model_linux.h"
 #include <backend/gpu/mali_kbase_model_dummy.h>
 #endif /* CONFIG_MALI_NO_MALI */
-#include "uapi/gpu/arm/midgard/mali_kbase_mem_profile_debugfs_buf_size.h"
+#include "uapi/gpu/arm/bv_r41p0/mali_kbase_mem_profile_debugfs_buf_size.h"
 #include "mali_kbase_mem.h"
 #include "mali_kbase_mem_pool_debugfs.h"
 #include "mali_kbase_mem_pool_group.h"
@@ -50,7 +50,7 @@
 #include <mali_kbase_hwaccess_instr.h>
 #endif
 #include <mali_kbase_reset_gpu.h>
-#include <uapi/gpu/arm/midgard/mali_kbase_ioctl.h>
+#include <uapi/gpu/arm/bv_r41p0/mali_kbase_ioctl.h>
 #if !MALI_USE_CSF
 #include "mali_kbase_kinstr_jm.h"
 #endif
@@ -2073,6 +2073,9 @@ static long kbase_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				kctx);
 		break;
 	}
+
+	if (!mali_exynos_ioctl(kctx, cmd, arg))
+		return 0;
 
 	dev_warn(kbdev->dev, "Unknown ioctl 0x%x nr:%d", cmd, _IOC_NR(cmd));
 
@@ -4298,6 +4301,8 @@ static int kbase_common_reg_map(struct kbase_device *kbdev)
 		goto out_ioremap;
 	}
 
+	mali_exynos_coherency_reg_map();
+
 	return err;
 
 out_ioremap:
@@ -4315,6 +4320,8 @@ static void kbase_common_reg_unmap(struct kbase_device * const kbdev)
 		kbdev->reg_start = 0;
 		kbdev->reg_size = 0;
 	}
+
+	mali_exynos_coherency_reg_unmap();
 }
 #endif /* CONFIG_MALI_NO_MALI */
 
@@ -5449,6 +5456,8 @@ int kbase_sysfs_init(struct kbase_device *kbdev)
 			&kbase_attr_group);
 	}
 
+	mali_exynos_sysfs_set_gpu_model_callback(&gpuinfo_show);
+
 	return err;
 }
 
@@ -5601,6 +5610,9 @@ static int kbase_device_resume(struct device *dev)
 	if (!kbdev)
 		return -ENODEV;
 
+	/* MALI_SEC_INTEGRATION */
+	mali_exynos_set_pm_state_resume_begin();
+
 	kbase_pm_resume(kbdev);
 
 #ifdef CONFIG_MALI_MIDGARD_DVFS
@@ -5612,6 +5624,10 @@ static int kbase_device_resume(struct device *dev)
 	if (kbdev->devfreq)
 		kbase_devfreq_enqueue_work(kbdev, DEVFREQ_WORK_RESUME);
 #endif
+
+	/* MALI_SEC_INTEGRATION */
+	mali_exynos_set_pm_state_resume_end();
+
 	return 0;
 }
 
@@ -5728,7 +5744,9 @@ static int kbase_device_runtime_idle(struct device *dev)
 	 * the autosuspend delay and so won't suspend the device immediately.
 	 */
 	pm_runtime_mark_last_busy(kbdev->dev);
-	return 0;
+	/* MALI_SEC_INTEGRATION */
+	/* Runtime IDLE must be return 1 for turn on next time by RuntimePM API!! */
+	return 1;
 }
 #endif /* KBASE_PM_RUNTIME */
 
@@ -5745,7 +5763,10 @@ static const struct dev_pm_ops kbase_pm_ops = {
 };
 
 #if IS_ENABLED(CONFIG_OF)
-static const struct of_device_id kbase_dt_ids[] = { { .compatible = "arm,malit6xx" },
+static const struct of_device_id kbase_dt_ids[] = {
+							/* MALI_SEC_INTEGRATION */
+							{ .compatible = "arm,mali", },
+							{ .compatible = "arm,malit6xx" },
 						    { .compatible = "arm,mali-midgard" },
 						    { .compatible = "arm,mali-bifrost" },
 						    { .compatible = "arm,mali-valhall" },
